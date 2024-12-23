9455b95626608761d393378c85e4ca03fa611234
joffutt-dev <joeoffutt4@gmail.com>
2024-12-22 23:37:11 -0500

    syncing changes before major updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Header } from "./components/Header";
+import { MainRouter } from "./containers/MainRouter";
+import "./styles/mainStyles.css";
+
 export function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <MainRouter />
+    </>
+  );
 }
---
+++ src/containers/Home.tsx
@@ -0,0 +1,8 @@
+export function Home() {
+  return (
+    <div>
+      This is the home page
+      <div>Carousel</div>
+    </div>
+  );
+}
